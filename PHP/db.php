4b59f30ce13dc72d90e08faba39fb22ba2758bca
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-26 22:38:01 -0500

    intento dos conexion base de datos

--- PHP/db.php
+++ PHP/db.php
@@ -1,3 +1,4 @@
 <?php 
-$conexion= mysqli_connect("localhost","root","Mi16","tiendavirtual");
+$conexion=mysqli_connect("localhost","root","Mi16","tiendavirtual");
 ?>
+<H1>CONECTADO</H1>
--- PHP/validar.php
+++ PHP/validar.php
@@ -1,21 +1,30 @@
 <?php
+include('db.php');
 $usuario=$_POST['usuario'];
 $contraseña=$_POST['contraseña'];
 session_start();
-$_SESSION['usuario']= $usuario;
-include('db.php');
-$consulta= "SELECT*FROM persona where  usuario= '$usuario' and contraseña='$contraseña' ";
-$resultado=mysqli_query($conexion,$cosnsulta);
-$filas=mysqli_num_rows($resultado); 
- if($filas){
-     header("location:inicio.html");
- }else{
-?>
-<?php 
-include("login.hmtl");
-?>
-<h1>Error en la autentificacion</h1>
-<?php
- }
- mysqli_free_result($resultado);
- mysqli_close($conexion);
+$_SESSION['usuario']=$usuario;
+
+
+$conexion=mysqli_connect("localhost","root","Mi16","tiendavirtual");
+
+$consulta="SELECT*FROM persona where usuario='$usuario' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion, $consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:inicio.html");
+
+}else{
+    ?>
+    <?php
+    include("login.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
