c44f9ed6873ce67fed1b65c46b84021ea4322dc0
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-27 13:48:36 -0500

    validar contraseña

--- PHP/db.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$conexion=mysqli_connect("localhost","root","Mi16","tiendavirtual");
-?>
-<H1>CONECTADO</H1>
--- javaScript/crearUsuario.js
+++ javaScript/crearUsuario.js
@@ -1,3 +1,23 @@
 function onclickGuardar(){
-    alert('Desea Guardar los datos del usuario');
+    var p1 = document.getElementById("contr").value;
+    var p2 = document.getElementById("contr1").value;
+    var espacios = false;
+    var cont = 0;
+    if(p1!=p2){
+        alert('Las contraseñas no coinciden')
+        onclickGuardar.caller=false;
+    }else{
+        if(p1=='' ||p2==''){
+            alert('El sistema no permite contraseñas vacias');
+
+        }
+        if(p1==' ' &&p2==' '){
+            alert('Asigne una contraseña sin espacios');
+        }
+        alert('Usuario creado');
+    }
+
+    
+
+   
 }
