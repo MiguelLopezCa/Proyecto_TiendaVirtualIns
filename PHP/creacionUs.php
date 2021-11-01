0d6d668331debb44acf90918d797a980fbba3aee
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-01 07:53:30 -0500

    validar base

--- PHP/creacionUs.php
+++ PHP/creacionUs.php
@@ -1,2 +1,10 @@
+<?php 
+$servidorDB = "localhost:3306";
+$nombreDb = "tiendavirtual";
+$usuarioDb = "root";
+$passwordDB = "";
 
+// Create connection
+$conn = new mysqli($servidorDB, $usuarioDb,$passwordDB,$nombreDb );
+?>
 
