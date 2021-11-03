8740d293a6957e69c66e1ae92ec811525492c68e
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-03 17:08:09 -0500

    instrumentos

---
+++ PHP/instrumentos.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Formulario de prueba</title>
+    <link rel="stylesheet" href="../CSS/instrumentos.css">
+    <link rel="stylesheet" href="../CSS/Inicio.css">
+    <link rel="shortcut icon" href="../resource/logo.jpg">
+    <link rel="stylesheet" href="../style.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
+</head>
+
+<body>
+    <header>
+    <div class="menu">
+            <a href="#" class="bt-menu"></a>
+
+            <nav>
+                <ul>
+                    <li><a href="#"><span class="icon-home"></span> Inicio</a></li>
+                    <li><a href="#"><span class="icon-mic"></span> ¿Quienes somos?</a></li>
+                    <li><a href="#"><span class="icon-music"></span> Instrumentos</a></li>
+                    <li><a href="#"><span class="icon-play"></span> Musica y más</a></li>
+                    <li><a href="#"><span class="icon-location"></span> Contacto</a></li>
+                    <div class="busc"><input type=" text " placeholder="Buscador " id="buscador "></div>
+                    <div class="imagen "><img src="../resource/logo.png " width="150px " height="75px "></div>
+                </ul>
+            </nav>
+            <div class="login"><a href="../PHP/login.php"><span class="icon-users"></span>Iniciar sesión o registrarse</a></div>
+
+        </div>
+        
+    </header>
+
+    <section>
+        <table style="border: 1 black;" >
+            <tr>
+                <td>
+                     
+                    <h1>Precio</h1>
+                </td>
+            </tr>
+        </table>
+    </section>
+    <footer>
+        &copy; 2021 Derechos reservados
+    </footer>
+</body>
+
+</html>
