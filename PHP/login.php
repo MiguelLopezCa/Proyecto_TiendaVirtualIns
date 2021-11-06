1ff68afbd3dd0b65315eefa63f535db21bc69643
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-05 21:42:25 -0500

    fondo login

--- PHP/login.php
+++ PHP/login.php
@@ -5,7 +5,7 @@
     <title>Ingresar al sistema</title>
     <link rel="stylesheet" href="../css/login.css">
 </head>
-<body>
+<body style=" background-image: url(../resource/fondo2.jpg) ;">>
     <div id="conten">
         <form action="validar.php" method="post">
             <h1 id="titulo">Iniciar sesión</h1>
