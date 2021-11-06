4f249567d5d8f741272c51dce09f8d3399720ad8
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-05 21:07:38 -0500

    comentariar if de rl de usuarios

--- PHP/validar.php
+++ PHP/validar.php
@@ -16,11 +16,11 @@ $resultado=mysqli_query($conexion, $consulta);
 $filas=mysqli_num_rows($resultado);
 
 if($filas){
-    if ($rol=='cliente'){
+    //if ($rol=='cliente'){
       header("location:../PHP/inicio.php");
-    }else{
-      header("location:../PHP/listaDeUsuarios.php");
-    }
+    //}else{
+     // header("location:../PHP/listaDeUsuarios.php");
+    //}
 
 }else{
     ?>
