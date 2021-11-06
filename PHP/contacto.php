457d81b13b143b4af07aa989392040fb0b47d63a
nicollejuli12 <nicollejuli@hotmail.com>
2021-11-05 22:20:50 -0500

    ok

--- PHP/contacto.php
+++ PHP/contacto.php
@@ -40,12 +40,7 @@ include '../HTML/menu.html';
     </div></div>
     <div class="contenedor">
             <div class="imagen" >
-    <img src="../resource/v.jpg"   height= 346px width= 300px>
-    </div></div>
-    <div class="contenedor">
-            <div class="imagen" >
-        <img src="../resource/gu.jpg"   height= 330px width= 300px>
-        
+    <img src="../resource/v.jpg"   height= 300px width= 300px>
     </div></div>
         </aside>
         <article>
@@ -56,11 +51,7 @@ include '../HTML/menu.html';
 <BR>
 <BR>
 <center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.3038911116228!2d-73.01941547084498!3d5.82519933391597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a3f6ac9393753%3A0x6ecf0b84f298191f!2sPueblito+Boyacense!5e0!3m2!1ses!2sco!4v1531976205147" width="1320" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></center>
-<font color="black" face="FROSTY">
-<HR>
-<CENTER><H5><FONT COLOR="NAVY"><FONT FACE="FROSTY"><I>SOLUCIONES RIVEIRA S.A. TODOS LOS DERECHOS RESERVADOS. 2018 ©</I></FONT FACE="FROSTY"></FONT COLOR="NAVY"></FORECOLOR"NAVY"></H5></CENTER>
-   </HR>
-         
+<font color="black" face="FROSTY">       
         </article>
         <aside id="Der">
             <img src="../resource/p.jpg"   height= 2000px
