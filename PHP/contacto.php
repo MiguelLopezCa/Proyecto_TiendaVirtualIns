e89cef4f993959608e84bc384e92f8141a103af0
nicollejuli12 <nicollejuli@hotmail.com>
2021-11-05 22:19:14 -0500

    ok

--- PHP/contacto.php
+++ PHP/contacto.php
@@ -23,25 +23,47 @@ include '../HTML/menu.html';
             <div class="imagen" >
     <img src="../resource/v.jpg"   height= 346px width= 300px>
     </div></div>
+    <div class="contenedor">
+            <div class="imagen" >
+        <img src="../resource/gu.jpg"   height= 330px width= 300px>
+        
+    </div></div>
+    
+    <div class="contenedor">
+            <div class="imagen" >
+    <img src="../resource/v.jpg"   height= 346px width= 300px>
+    </div></div>
+    <div class="contenedor">
+            <div class="imagen" >
+        <img src="../resource/gu.jpg"   height= 330px width= 300px>
+        
+    </div></div>
+    <div class="contenedor">
+            <div class="imagen" >
+    <img src="../resource/v.jpg"   height= 346px width= 300px>
+    </div></div>
+    <div class="contenedor">
+            <div class="imagen" >
+        <img src="../resource/gu.jpg"   height= 330px width= 300px>
+        
+    </div></div>
         </aside>
         <article>
-            <!-- <div class="video">
-      
-
-<video width="500" height="300" controls autoplay>
-  	<source src="../resource/l1.mp4" type="video/mp4">
-</video>
-            </div> -->
-            <table>
-                <tr>
-                <th>Misión</th>
-                <th>Visión</th>
-                </tr>
-                
-            </table>
+<br>
+<br>
+<br>
+<center><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRUB4L_4nnA1iBT8cAbLgSEJ3N2c1MmWKX26qZtvRou2r9bQ/viewform?embedded=true" width="1300" height="1700" frameborder="6O" marginheight="100" marginwidth="100">Cargando...</iframe></center>
+<BR>
+<BR>
+<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.3038911116228!2d-73.01941547084498!3d5.82519933391597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a3f6ac9393753%3A0x6ecf0b84f298191f!2sPueblito+Boyacense!5e0!3m2!1ses!2sco!4v1531976205147" width="1320" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></center>
+<font color="black" face="FROSTY">
+<HR>
+<CENTER><H5><FONT COLOR="NAVY"><FONT FACE="FROSTY"><I>SOLUCIONES RIVEIRA S.A. TODOS LOS DERECHOS RESERVADOS. 2018 ©</I></FONT FACE="FROSTY"></FONT COLOR="NAVY"></FORECOLOR"NAVY"></H5></CENTER>
+   </HR>
+         
         </article>
         <aside id="Der">
-            <img src="../resource/p.jpg"   height= 683px
+            <img src="../resource/p.jpg"   height= 2000px
     width= 300px>
         </aside>
     </section>
--- PHP/musica.php
+++ PHP/musica.php
@@ -7,7 +7,7 @@ include '../HTML/menu.html';
 <head>
     <meta charset="UTF-8" />
     <title>Música y más</title>  
-    <link rel="stylesheet" href="../CSS/contacto.css">  
+    <link rel="stylesheet" href="../CSS/musica.css">  
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
 </head>
 
