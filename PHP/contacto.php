72d60c5f7c1118f40ded8a623d487483c835136b
nicollejuli12 <nicollejuli@hotmail.com>
2021-11-05 22:35:14 -0500

    ok

--- PHP/contacto.php
+++ PHP/contacto.php
@@ -47,8 +47,9 @@ include '../HTML/menu.html';
 <br>
 <br>
 <br>
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRUB4L_4nnA1iBT8cAbLgSEJ3N2c1MmWKX26qZtvRou2r9bQ/viewform?embedded=true" width="1000" height="1700" frameborder="6O" marginheight="100" marginwidth="100">Cargando...</iframe>
-<BR>
+<div class="form"><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRUB4L_4nnA1iBT8cAbLgSEJ3N2c1MmWKX26qZtvRou2r9bQ/viewform?embedded=true" width="1000" height="1700" frameborder="6O">Cargando...</iframe>
+    </div>
+    <BR>
 <BR>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.3038911116228!2d-73.01941547084498!3d5.82519933391597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a3f6ac9393753%3A0x6ecf0b84f298191f!2sPueblito+Boyacense!5e0!3m2!1ses!2sco!4v1531976205147" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
   
