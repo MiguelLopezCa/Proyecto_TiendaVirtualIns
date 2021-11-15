a1cad566ef101ea5b036a2360bf6512b478e672c
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-14 21:08:22 -0500

    Cambios de css

--- PHP/quienes.php
+++ PHP/quienes.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>¿Quienes somos?</title>
-    <link rel="stylesheet" href="../CSS/quienes.css">
+    <link rel="stylesheet" href="../CSS/quienesS.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
     <meta charset="utf-8" />
     <title>Inicio</title>
