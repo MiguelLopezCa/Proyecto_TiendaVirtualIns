fc20b19db8d9c4f23614143eea529efbebf488b9
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-18 00:57:22 -0500

    cambio de nombre del css de instrumentos

--- PHP/instrumentos.php
+++ PHP/instrumentos.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-<link rel="stylesheet" href="../CSS/instrumentos.css">
+<link rel="stylesheet" href="../CSS/instrumentoss.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
     <meta charset="utf-8" />
     <title>Inicio</title>
