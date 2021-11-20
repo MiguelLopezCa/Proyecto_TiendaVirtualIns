e828429a8f006797535a8cc2f003d2c760ca8457
nicollejuli12 <nicollejuli@hotmail.com>
2021-11-20 14:16:36 -0500

    Update contacto.php

--- PHP/contacto.php
+++ PHP/contacto.php
@@ -59,8 +59,8 @@ include '../HTML/menu.html';
     </div>
     <BR>
 <BR>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.3038911116228!2d-73.01941547084498!3d5.82519933391597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a3f6ac9393753%3A0x6ecf0b84f298191f!2sPueblito+Boyacense!5e0!3m2!1ses!2sco!4v1531976205147" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-  
+<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992.3038911116228!2d-73.01941547084498!3d5.82519933391597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a3f6ac9393753%3A0x6ecf0b84f298191f!2sPueblito+Boyacense!5e0!3m2!1ses!2sco!4v1531976205147" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdOCG1fst56ifbFaFQTz3p9T2tpZNEF7MXwyS5A5bLSFPlv5Q/viewform?embedded=true" width="900" height="200" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </article>
         <aside id="Der">
             <img src="../resource/p.jpg"   height= 2000px
