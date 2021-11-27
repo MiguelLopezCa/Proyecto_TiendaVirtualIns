6c19ee54fca274c2356d47716c5e8b6249e15fc9
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-11-27 10:05:55 -0500

    Validacions de creacion de ususarios

--- PHP/creacionUsuario.php
+++ PHP/creacionUsuario.php
@@ -19,7 +19,7 @@ $persona =[
     <meta charset="UTF-8">
     <title>Crear usuario</title>
     <link rel="shortcut icon" href="../resource/logo.jpg">
-    <link rel="stylesheet" href="../css/ceracionUsua.css">
+    <link rel="stylesheet" href="../css/ceracionUsuas.css">
 </head>
 <body style=" background-image: url(../resource/fondo1.jpg) ;">
 
@@ -27,7 +27,7 @@ $persona =[
     </br>
    
     </br></br>
-    <form action="<?php echo $actionForm;?>" method="POST">
+    <form class="formulario" id="formulario" action="<?php echo $actionForm;?>" method="POST">
     <?php
     if (!empty($identificacion)){
         $servidorDB = "localhost:3306";
@@ -56,33 +56,59 @@ $persona =[
     }
 
     ?>  
-    <form >
+    <form>
         <div id="divCont">
-        <div>
+		<form action="" class="formulario" id="formulario">	
+		<div class="formulario__grupo divDatos" id="grupo__nombre">
             <label class="lblDatos" id="LblNombre" for="nombre"> Nombre</label>
-            <input class="inpDatos" id="nombre" name="nombre" type="text" value="<?php echo$persona['nombres'] ?>" placeholder="Ingrese su nombre" required />
-        </div> 
-        <div>
+		<div class="formulario__grupo-input">
+			<input class="inpDatos formulario__input " id="nombre" name="nombre" type="text" value="<?php echo$persona['nombres'] ?>" placeholder="Ingrese su nombre" required />
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>
+		</div> 
+			<p class="formulario__input-error">El nombre tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+			
+		</div> 
+        <div class="formulario__grupo divDatos" id="grupo__apellido">
             <label class="lblDatos" id="lblApellido" for="apellido"> Apellido</label>
-            <input class="inpDatos" id="apellido" name="apellido" type="text" value="<?php echo$persona['apellidos'] ?>" placeholder="Ingrese su apellido" required />
-        </div>
-        <div>
+        <div class="formulario__grupo-input">
+			<input class="inpDatos formulario__input" id="apellido" name="apellido" type="text" value="<?php echo$persona['apellidos'] ?>" placeholder="Ingrese su apellido" required />
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>
+			</div>
+			<p class="formulario__input-error">El apellido tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+			
+		</div>
+		<div class="formulario__grupo divDatos" id="grupo__identificacion">
             <label class="lblDatos" id="lblIden" for="ident"> Identificación </label>
-            <input class="inpDatos" id="identificacion" name="identificacion" type="number" value="<?php echo$persona['identificacion'] ?>" placeholder="Ingrese su Identificación" required/>
-                        
+		<div class="formulario__grupo-input">
+			<input class="inpDatos formulario__input" id="identificacion" name="identificacion" type="number" value="<?php echo$persona['identificacion'] ?>" placeholder="Ingrese su Identificación" required/>
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>
+		</div>
+			<p class="formulario__input-error">La IDENTIFICACION tiene que ser de 8 a 10 dígitos.</p>         
         </div>
-        <div>
+		<div class="formulario__grupo divDatos" id="grupo__password">
             <label class="lblDatos" id="contra" for="contraseña"> contraseña </label>
-            <input class="inpDatos" id="contraseña" name="contraseña" type="password" value="<?php echo$persona['contraseña'] ?>" placeholder="Ingrese su contraseña" required />
+		<div class="formulario__grupo-input"> 
+			<input class="inpDatos" id="contraseña" name="contraseña" type="password" value="<?php echo$persona['contraseña'] ?>" placeholder="Ingrese su contraseña" required />
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>
+		</div>
+			<p class="formulario__input-error">La IDENTIFICACION tiene que ser de 8 a 10 dígitos.</p>         
         </div>
-        <div>
+		<div class="formulario__grupo divDatos" id="grupo__password2">
             <label class="lblDatos" id="contra1" for="contraseña1"> contraseña </label>
-            <input class="inpDatos" id="contraseña1" name="contraseña1" type="password"  value="<?php echo$persona['contraseña'] ?>" placeholder="Repita su contraseña" required/>
-        </div> 
-        <div>
-        <label class="lblDatos" id="lblUsuario" for="usuario"> Usuario</label>
-        <input class="inpDatos" id="usuario" name="usuario" type="text" value="<?php echo$persona['usuario'] ?>" placeholder="Ingrese su usuario" required />
-        </div>              
+		<div class="formulario__grupo-input">   
+			<input class="inpDatos" id="contraseña1" name="contraseña1" type="password"  value="<?php echo$persona['contraseña'] ?>" placeholder="Repita su contraseña" required/>
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>
+		</div> 
+			<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+		</div> 
+		<div class="formulario__grupo divDatos" id="grupo__usuario">
+       		 <label class="lblDatos" id="lblUsuario" for="usuario"> Usuario</label>
+        <div class="formulario__grupo-input"> 
+			<input class="inpDatos" id="usuario" name="usuario" type="text" value="<?php echo$persona['usuario'] ?>" placeholder="Ingrese su usuario" required />
+			<i class="formulario__validacion-estado fas fa-times-circle"></i>	
+		</div> 
+		<p class="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+		</div>              
         <div>
         <label class="lblDatos" id="lblRol" for="rol"> Rol del usuario</label>
         <select class="inpDatos" id="rol"  name="rol" required value="<?php echo$persona['rol'] ?>">
@@ -90,23 +116,26 @@ $persona =[
                 <option value="cliente">Cliente  </option>
                 <option value="administrador"> Administrador </option>
         </select>
-        </div>                    
-        <div>
+        </div>
+
+       <div class="formulario__grupo" id="grupo__telefono">
         <label class="inpDatos" id="tel" for="telefono"> Telefono </label>
-        <input  class="inpDatos" id="telefono" name="telefono" type="number" value="<?php echo$persona['telefono'] ?>" placeholder="Ingrese su telefono" required />
-        </div>    
+        <div class="formulario__grupo-input"> 
+		<input  class="inpDatos" id="telefono" name="telefono" type="number" value="<?php echo$persona['telefono'] ?>" placeholder="Ingrese su telefono" required />
+		<i class="formulario__validacion-estado fas fa-times-circle"></i>
+		</div>
+		<p class="formulario__input-error">El telefono solo puede contener numeros y el maximo son 14 dígitos.</p>
+			    
+	</div>    
             
             <button  id="btnGuardar" class="inpDatos btnCr"  name="submit" type="submit" >Guardar</button>
                     <button id="btnCancelar" class="inpDatos btnCr" type="reset">Cancelar</button>
                     <button id="btnRegresar" class="inpDatos btnCr" onclick="window.location.href='../PHP/login.php'" type="button">Regresar</button>
-          <?php 
         
-          include '../JavaScript/crearUsuario.php';
-          
-          ?>           
-    </form>
+   
    
 </div>
+</form>
 <script type="text/javascript" src="../javaScript/crearUsuario.js"></script>
 </body>
  </html>
